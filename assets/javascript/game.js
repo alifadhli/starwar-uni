500ac0ed30f66701a65046818b13f0e9ce9b5556
alifadhli <mr_86_ha_mo@yahoo.com>
2017-05-10 19:40:53 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ let characters = {
         name: 'Princess Leia',
         health: 140,
         attack: 15,
-        imageUrl: "assets/images/Princess-Leia.jpg",
+        imageUrl: "assets/images/Princess-Leia.Jpg",
         enemyAttackBack: 18
     }, 
     'Greedo': {
